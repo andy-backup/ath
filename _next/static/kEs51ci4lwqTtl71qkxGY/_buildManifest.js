6a5f6421ccabdbd4a375a911c1623c3dd5ad8056
mingfunwong <mingfunwong@users.noreply.github.com>
2021-03-25 10:15:20 +0000

    deploy: 3fc791e0edfde08a961ff169de29d87907dbff3d

---
+++ _next/static/kEs51ci4lwqTtl71qkxGY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-7bf475269d8d9565545d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-38604202843cbf9a7ce2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
