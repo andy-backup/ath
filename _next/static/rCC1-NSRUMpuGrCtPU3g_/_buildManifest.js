4a0975a86ff33d84e3228dd7d02460d6a6c795b7
mingfunwong <mingfunwong@users.noreply.github.com>
2021-11-02 09:23:10 +0000

    deploy: e8a547d7d7c1f5dcc2bcdf12e2b9915ca7a71e66

--- _next/static/rCC1-NSRUMpuGrCtPU3g_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-4bd4317cd1808ee8b92e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-38604202843cbf9a7ce2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
